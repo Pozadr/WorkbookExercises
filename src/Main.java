203ea6a5c6d0601641955a5ada2e21bd782798cf
Pozadr <adpozor@gmail.com>
2020-04-25 15:23:45 +0200

    Task3 finished

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
 public class Main {
     public static void main(String[] args){
+        // Task 1, 2
         Task1_2.display();
-        Task3 test = new Task3();
-        test.displayA(8);
-        test.displayB(8);
-        test.displayC(8);
-        test.displayD(8);
-        test.displayE(8);
-        test.displayF(7);
-        test.displayG(7);
-        test.displayH(7);
+        // Task3
+        Task3 task3 = new Task3();
+        task3.displayA(8);
+        task3.displayB(8);
+        task3.displayC(8);
+        task3.displayD(8);
+        task3.displayE(8);
+        task3.displayF(7);
+        task3.displayG(7);
+        task3.displayH(7);
+        task3.displayI(7);
 
 
     }
--- src/Task3.java
+++ src/Task3.java
@@ -151,7 +151,27 @@ public class Task3 {
 
 
     // Square with diagonals
-
+    public void displayI(int size){
+        System.out.println();
+        for(int i=1; i<=size; i++){
+            if(i==1 || i == size){
+                for (int j=1; j<=size; j++) {
+                    System.out.print("# ");
+                }
+            }
+            else {
+                for (int j=1; j<=size; j++) {
+                    if (j == 1 || j == size || j == i
+                            || j == size - i + 1) {
+                        System.out.print("# ");
+                    } else {
+                        System.out.print(". ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
 
 
 }
