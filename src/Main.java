cdf2b4f1eb6a066310318bf5056de2dcfb9eafb1
Pozadr <adpozor@gmail.com>
2020-04-27 13:18:28 +0200

    Task5 finished

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args){
         // Task 1, 2
+        System.out.println("\nTask1,2");
         Task1_2.display();
         // Task3
+        System.out.println("\nTask3");
         Task3 task3 = new Task3();
         task3.displayA(8);
         task3.displayB(8);
@@ -14,12 +16,16 @@ public class Main {
         task3.displayH(7);
         task3.displayI(7);
         // Task4
-        System.out.println();
+        System.out.println("\nTask4");
         Task4 task4 = new Task4();
         String numberToReduce = "202020";
         int reducedValue = task4.reduce(numberToReduce);
         System.out.println("Number to reduce: " + numberToReduce);
         System.out.println("After reduce: " + reducedValue);
+        // Task5
+        System.out.println("\nTask5");
+        Task5 task5 = new Task5();
+        task5.displayWhoWon("0X_O0XO_X");
 
 
     }
---
+++ src/Task5.java
@@ -0,0 +1,71 @@
+public class Task5 {
+
+    public void displayWhoWon(String gameStatus){
+        String result = "";
+
+        //try cauch do zrobienia ArrayIndexOutOfBoundsException
+        String[] inputToArray = new String [9];
+        inputToArray = gameStatus.split("");
+
+        // write game status to two dimensional array
+        String [][] twoDimensionalArray = new String [3][3];
+        int inputToArrayCounter = 0;
+        for (int i = 0; i < twoDimensionalArray.length; i++) {
+            for (int j = 0; j < twoDimensionalArray[i].length; j++) {
+                // check and convert: x, o, X, O
+                if (inputToArray[inputToArrayCounter].equals("x")){
+                    inputToArray[inputToArrayCounter] = "X";
+                }
+                else if (inputToArray[inputToArrayCounter].equals("o")
+                    || inputToArray[inputToArrayCounter].equals("0")){
+                    inputToArray[inputToArrayCounter] = "O";
+                }
+                // write sign to two dimensional array
+                twoDimensionalArray[i][j] = inputToArray[inputToArrayCounter];
+                //display game status from two dimensional array
+                System.out.print(twoDimensionalArray[i][j]);
+                //increase one dimensional array iterator
+                inputToArrayCounter++;
+            }
+            // go to the next row for next 'i'
+            System.out.println();
+        }
+
+        // check
+        for (int i = 0; i < twoDimensionalArray.length; i++) {
+            // check rows
+            if (twoDimensionalArray[i][0].equals(twoDimensionalArray[i][1])
+                    && twoDimensionalArray[i][1].equals(twoDimensionalArray[i][2])) {
+                result = twoDimensionalArray[i][i];
+            }
+            // check columns
+            else if (twoDimensionalArray[0][i].equals(twoDimensionalArray[1][i])
+                    && twoDimensionalArray[1][i].equals(twoDimensionalArray[2][i])) {
+                result = twoDimensionalArray[i][i];
+            }
+        }
+
+        // check diagonals
+        if (twoDimensionalArray[0][0].equals(twoDimensionalArray[1][1])
+                && twoDimensionalArray[1][1].equals(twoDimensionalArray[2][2])) {
+            result = twoDimensionalArray[0][0];
+        }
+        else if (twoDimensionalArray[0][2].equals(twoDimensionalArray[1][1])
+                && twoDimensionalArray[1][1].equals(twoDimensionalArray[2][0])) {
+            result = twoDimensionalArray[1][1];
+        }
+
+        // display result
+        if (!result.equals("")){
+            System.out.println("The winner is: " + result);
+        }
+        else {
+            System.out.println("There is a draw!");
+        }
+
+
+
+    }
+
+
+}
