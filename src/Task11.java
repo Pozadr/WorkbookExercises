9ef64b890aaa23ac97689cdde5c4917837c9ca71
Pozadr <adpozor@gmail.com>
2020-04-29 13:15:35 +0200

    Task11 init

---
+++ src/Task11.java
@@ -0,0 +1,22 @@
+public class Task11 {
+    private final int[] array;
+
+    public Task11(int arrayLenght) {
+        this.array =  new int[arrayLenght];
+    }
+
+    public void add(Integer value) {
+    }
+    public void add(Integer value, int idx) {
+    }
+    public Integer get(int idx) {
+        return null;
+    }
+    public void remove(int idx) {
+    }
+    public void swap(int from, int to) {
+    }
+    public String toString() {
+        return "";
+    }
+}
